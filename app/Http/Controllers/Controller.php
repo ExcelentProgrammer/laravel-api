abc77a57cb8a4764bc933dc70ac5b09c4d146fc9
Azamov Samandar <azamov.samandar.programmer@gmail.com>
2023-12-20 14:30:32 +0500

    update

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Routing\Controller as BController;
 
-class Controller extends BaseController
+class Controller extends BController
 {
-    use AuthorizesRequests, ValidatesRequests, \App\Http\Controllers\BaseController;
+    use AuthorizesRequests, ValidatesRequests, BaseController;
 }
