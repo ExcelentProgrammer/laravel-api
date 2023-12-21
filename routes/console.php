cd7b024cd09558144a7394092776a688d419ea7d
Azamov Samandar <azamov.samandar.programmer@gmail.com>
2023-12-21 21:53:08 +0500

    update

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+Artisan::command("telegram",function (){
+    \App\Services\TelegramService::start("public/me.session");
+});
