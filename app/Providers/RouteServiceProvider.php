8898c6345e99f178d597613dde09036e81e6329c
Azamov Samandar <azamov.samandar.programmer@gmail.com>
2023-12-26 22:26:43 +0500

    update

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -27,9 +27,7 @@ class RouteServiceProvider extends ServiceProvider
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
 
-            Route::middleware("web")
-                ->prefix("admin")
-                ->group(base_path("routes/admin.php"));
+         
 
             Route::middleware("api")
                 ->prefix("api/auth")
