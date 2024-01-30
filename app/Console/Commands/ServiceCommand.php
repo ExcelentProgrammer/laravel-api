e57f7b71a9a88eb9f9603d1ee240d67f3b678bc1
Azamov Samandar <azamov.samandar.programmer@gmail.com>
2024-01-30 17:59:35 +0500

    update

--- app/Console/Commands/ServiceCommand.php
+++ app/Console/Commands/ServiceCommand.php
@@ -13,7 +13,7 @@ class ServiceCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'make:service {name} {model} {resource}';
+    protected $signature = 'make:service {name}';
 
     /**
      * The console command description.
@@ -24,20 +24,48 @@ class ServiceCommand extends Command
 
     public function handle(): void
     {
+
+
+        $module = null;
         $name = $this->argument('name');
 
-        $model = $this->argument('model');
-        $resource = $this->argument('resource');
+
+        while (true) {
+            $type = $this->ask("module or base m/B: ") ?? "b";
+            if ($type == "m" or $type == "b") {
+                break;
+            }
+        }
+        if ($type == "m") {
+            while (true) {
+                $module = $this->ask("Enter Module name: ") ?? null;
+                if ($module != null and file_exists("Modules/" . $module)) {
+                    break;
+                }
+                $this->error("Module not found");
+            }
+        }
+
+        $model = $this->ask("Enter Model name: ") ?? "BaseModel";
+        $resource = $this->ask("Enter resource name: ") ?? "BaseResource";
 
         $className = Str::studly($name) . 'Service';
-        $path = app_path("Services/{$className}.php");
+
+        if ($type == "m") {
+            if (!file_exists("Modules/$module/App/Services")) {
+                mkdir("Modules/$module/App/Services");
+            }
+            $path = base_path("Modules/$module/App/Services/{$className}.php");
+        } else {
+            $path = app_path("Services/{$className}.php");
+        }
 
         if (File::exists($path)) {
             $this->error('Service already exists!');
             return;
         }
 
-        File::put($path, $this->generateServiceClass($className, $model, $resource));
+        File::put($path, $this->generateServiceClass($className, $model, $resource, $module, $type));
 
         $this->info("Service {$className} created successfully!");
     }
@@ -46,11 +74,19 @@ class ServiceCommand extends Command
      * Generate service
      *
      * @param $className
+     * @param $model
+     * @param $resource
+     * @param $module
+     * @param $type
      * @return array|bool|string
      */
-    protected function generateServiceClass($className, $model, $resource): array|bool|string
+    protected function generateServiceClass($className, $model, $resource, $module, $type): array|bool|string
     {
-        $stub = file_get_contents(base_path('stubs/service.stub'));
-        return str_replace("{{resource}}", $resource, str_replace("{{model}}", $model, str_replace('{{class}}', $className, $stub)));
+        $path = match ($type) {
+            "b" => 'stubs/service.stub',
+            "m" => 'stubs/module-service.stub'
+        };
+        $stub = file_get_contents(base_path($path));
+        return str_replace("{{module}}", $module, str_replace("{{resource}}", $resource, str_replace("{{model}}", $model, str_replace('{{class}}', $className, $stub))));
     }
 }
