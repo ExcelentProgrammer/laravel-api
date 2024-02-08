8e2c6fc7447f0f63a80a03b6bfb04a1d577c499f
Azamov Samandar <azamov.samandar.programmer@gmail.com>
2024-02-08 18:04:07 +0500

    update service generator

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -12,116 +12,148 @@ use App\Http\Requests\Api\ResetRequest;
 use App\Http\Requests\Api\SetPasswordRequest;
 use App\Http\Requests\Api\UserUpdateRequest;
 use App\Http\Resources\Api\MeResource;
-use App\Models\User;
+use App\Services\Auth\AuthService;
 use App\Services\Sms\SmsService;
-use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Throwable;
 
+
+/**
+ * @tags Authorization
+ */
 class AuthController extends Controller
 {
     use BaseController;
 
+    public AuthService $service;
+
     public function __construct()
     {
         $this->middleware("auth:sanctum")->only(["me", "setPassword"]);
+        $this->service = new AuthService();
     }
 
+    /**
+     * Register new user
+     *
+     * @param RegisterRequest $request
+     * @return JsonResponse
+     * @response array{success:true,message:string}
+     */
     function register(RegisterRequest $request): JsonResponse
     {
         $phone = $request->input("phone");
         try {
-            $res = SmsService::sendConfirm($phone);
-            return $this->success(__("sms.send:success"), data: [
-                "provider" => $res === "telegram" ? $res : "sms",
-            ]);
-        } catch (\Throwable $e) {
+            SmsService::sendConfirm($phone);
+            return $this->success(__("sms.send:success"));
+        } catch (Throwable $e) {
             return $this->error($e->getMessage());
         }
     }
 
+    /**
+     * Confirm otp
+     *
+     * @param ConfirmRequest $request
+     * @return JsonResponse
+     * @response array{success:true,data:array{token:string}}
+     */
     function confirm(ConfirmRequest $request): JsonResponse
     {
         $code = $request->input("code");
         $phone = $request->input("phone");
         try {
-            $res = SmsService::checkConfirm($phone, $code);
-            if ($res) {
-                $user = User::query()->where(['phone' => $phone]);
-                if (!$user->exists()) {
-                    $user = User::query()->create([
-                        'phone' => $phone
-                    ]);
-                } else {
-                    $user = $user->first();
-                }
-
-                $token = $user->createToken(Carbon::now()->format("d.m.Y H:i"))->plainTextToken;
-                return $this->success(
-                    message: __("sms.confirm"),
-                    data: [
-                        "provider" => $res === "telegram" ? $res : "sms",
-                        "token" => $token
-                    ]
-                );
-            } else {
-                return $this->error(__("invalid:error"));
-            }
-        } catch (\Throwable $e) {
+            return $this->service->confirm($phone, $code);
+        } catch (Throwable $e) {
             return $this->error($e->getMessage());
         }
     }
 
+    /**
+     * Login
+     *
+     * @param LoginRequest $request
+     * @return JsonResponse
+     * @response array{success:true,data:array{token:string}}
+     */
     function login(LoginRequest $request): JsonResponse
     {
         $phone = $request->input("phone");
         $password = $request->input("password");
 
-        $user = User::query()->where(['phone' => $phone])->first();
-
-        if ($user->password == null or !Hash::check($password, $user->password)) {
-            return $this->error(__("invalid:password"));
+        try {
+            $token = $this->service->login($phone, $password);
+            return $this->success(data: [
+                "token" => $token
+            ]);
+        } catch (Throwable $e) {
+            return $this->error($e->getMessage());
         }
-        $token = $user->createToken("Base")->plainTextToken;
-        return $this->success(data: [
-            "token" => $token
-        ]);
+
     }
 
+    /**
+     * Get user data
+     *
+     * @return JsonResponse
+     *
+     * @response MeResource
+     */
     function me(): JsonResponse
     {
-
         return $this->success(data: MeResource::make(Auth::user()));
     }
 
+    /**
+     * Update user data
+     *
+     * @param UserUpdateRequest $request
+     * @return JsonResponse
+     * @response array{success:true,message:string}
+     */
     function update(UserUpdateRequest $request): JsonResponse
     {
         try {
-
             $user = Auth::user();
             $user->fill($request->validated());
             $user->save();
             return $this->success(__("user.update:profile"));
-
-        } catch (\Throwable $e) {
+        } catch (Throwable $e) {
             return $this->error($e->getMessage());
         }
     }
 
+    /**
+     * Reset password
+     *
+     * Reset user password
+     *
+     * @param ResetRequest $request
+     * @return JsonResponse
+     *
+     * @response array{success:true,message:string}
+     */
     function reset(ResetRequest $request): JsonResponse
     {
         $phone = $request->input("phone");
         try {
-            $res = SmsService::sendConfirm($phone);
-            return $this->success(__("sms.send:success"), data: [
-                "provider" => $res === "telegram" ? $res : "sms"
-            ]);
-        } catch (\Throwable $e) {
+            SmsService::sendConfirm($phone);
+            return $this->success(__("sms.send:success"));
+        } catch (Throwable $e) {
             return $this->error($e->getMessage());
         }
     }
 
+    /**
+     * Reset otp confirm
+     *
+     * @param ResetConfirmRequest $request
+     * @return JsonResponse
+     *
+     * @response array{success:true,message:string}
+     */
     function resetConfirm(ResetConfirmRequest $request): JsonResponse
     {
         $phone = $request->input("phone");
@@ -129,19 +161,21 @@ class AuthController extends Controller
         $password = $request->input("password");
 
         try {
-            $check = SmsService::checkConfirm($phone, $code);
-            if ($check == true) {
-                User::query()->where(['phone' => $phone])->update(['password' => Hash::make($password)]);
-                return $this->success(__("reset:password:done"));
-            }
-            return $this->error(__("reset:password:error"));
-
-        } catch (\Throwable $e) {
+            return $this->service->resetConfirm($phone, $code, $password);
+        } catch (Throwable $e) {
             return $this->error($e->getMessage());
         }
 
     }
 
+    /**
+     * Change or set user password
+     *
+     * @param SetPasswordRequest $request
+     * @return JsonResponse
+     *
+     * @response array{success:true,message:string}
+     */
     function setPassword(SetPasswordRequest $request): JsonResponse
     {
         $password = $request->input("password");
@@ -151,11 +185,9 @@ class AuthController extends Controller
             $user->save();
             return $this->success(__("set:password:done"));
 
-        } catch (\Throwable $e) {
+        } catch (Throwable $e) {
             return $this->error($e->getMessage());
         }
     }
-
-
 }
 
---
+++ app/Services/Auth/AuthService.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Services\Auth;
+
+use App\Exceptions\InvalidConfirmationCodeException;
+use App\Exceptions\IsBlockException;
+use App\Exceptions\IsExpiredException;
+use App\Exceptions\SmsNotFoundException;
+use App\Http\Controllers\BaseController;
+use App\Models\User;
+use App\Services\Sms\SmsService;
+use Carbon\Carbon;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Hash;
+
+class AuthService
+{
+    use BaseController;
+
+    /**
+     * Confirm otp code
+     *
+     * @throws IsBlockException
+     * @throws SmsNotFoundException
+     * @throws InvalidConfirmationCodeException
+     * @throws IsExpiredException
+     * @author Azamov Samandar
+     */
+    public function confirm($phone, $code): JsonResponse
+    {
+        $res = SmsService::checkConfirm($phone, $code);
+        if ($res) {
+            $user = User::query()->where(['phone' => $phone]);
+            if (!$user->exists()) {
+                $user = User::query()->create([
+                    'phone' => $phone
+                ]);
+            } else {
+                $user = $user->first();
+            }
+
+            $token = $user->createToken(Carbon::now()->format("d.m.Y H:i"))->plainTextToken;
+            return $this->success(
+                message: __("sms.confirm"),
+                data: [
+                    "token" => $token
+                ]
+            );
+        } else {
+            return $this->error(__("invalid:error"));
+        }
+    }
+
+    /**
+     * Login
+     *
+     * @throws Exception
+     */
+    public function login($phone, $password): string
+    {
+        $user = User::query()->where(['phone' => $phone])->first();
+
+        if ($user->password == null or !Hash::check($password, $user->password)) {
+            throw new Exception(__("invalid:password"));
+        }
+        return $user->createToken("Base")->plainTextToken;
+    }
+
+    /**
+     * Reset password otp confirm
+     *
+     * @param $phone
+     * @param $code
+     * @param $password
+     * @return JsonResponse
+     * @throws InvalidConfirmationCodeException
+     * @throws IsBlockException
+     * @throws IsExpiredException
+     * @throws SmsNotFoundException*@throws Exception
+     * @throws Exception
+     * @author Azamov Samandar
+     *
+     */
+    public function resetConfirm($phone, $code, $password): JsonResponse
+    {
+        $check = SmsService::checkConfirm($phone, $code);
+        if ($check) {
+            User::query()->where(['phone' => $phone])->update(['password' => Hash::make($password)]);
+            return $this->success(__("reset:password:done"));
+        }
+        throw new Exception(__("reset:password:error"));
+    }
+
+}
