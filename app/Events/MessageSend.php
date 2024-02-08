54a72686d56b48f6024b673a2fd6031f9b405f1d
Azamov Samandar <azamov.samandar.programmer@gmail.com>
2024-02-08 13:34:10 +0500

    update service generator

--- app/Events/MessageSend.php
+++ app/Events/MessageSend.php
@@ -4,12 +4,11 @@ namespace App\Events;
 
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class MessageSend implements ShouldBroadcast
+class MessageSend implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
